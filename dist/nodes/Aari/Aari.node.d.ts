f2d1fe12558ec9c6c85409a98426d2703655afeb
AARI <hello@getaari.com>
2026-03-20 07:34:25 +0000

    chore: remove node_modules and dist from tracking, add .gitignore

--- dist/nodes/Aari/Aari.node.d.ts
+++
@@ -1,6 +0,0 @@
-import type { IExecuteFunctions, INodeExecutionData, INodeType, INodeTypeDescription } from "n8n-workflow";
-export declare class Aari implements INodeType {
-    description: INodeTypeDescription;
-    execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]>;
-}
-//# sourceMappingURL=Aari.node.d.ts.map
